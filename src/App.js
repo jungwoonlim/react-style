abc5f7ded8ec96579109d56cea966ed60dc0eb85
jungwoonlim <wypo23@naver.com>
2019-06-26 17:43:26 +0900

    Theming

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css } from "styled-components";
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <GlobalStyle />
-        <Container>
-          <Input placeholder="Write plz" />
-        </Container>
-      </React.Fragment>
-    );
-  }
-}
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 const Container = styled.div`
   height: 100vh;
@@ -21,25 +9,39 @@ const Container = styled.div`
 `;
 
 const GlobalStyle = createGlobalStyle`
-  body{
-    padding: 0;
-    margin: 0;
+body{
+  padding: 0;
+  margin: 0;
+}
+`;
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyle />
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
   }
-`;
+}
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  border-radius: 5px;
-  ${awesomeCard}
-`;
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 export default App;
